cd2d5f52f4b7b89c5e393758453ac55543d957fb
Eric Jung <118792303+e11b@users.noreply.github.com>
2023-01-21 12:34:03 -0600

    Ex39 Printer encapsulation challenge

---
+++ Section8_OOP2_Composition/src/Ex_39/Printer.java
@@ -0,0 +1,40 @@
+package Ex_39;
+
+public class Printer {
+
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 || tonerLevel <= 100){
+            this.tonerLevel = tonerLevel;
+        } else this.tonerLevel = -1;
+        this.duplex = duplex;
+    }
+
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.pagesPrinted = 0 + pagesPrinted;
+        this.duplex = duplex;
+    }
+
+    public int addToner (int tonerAmount){
+        if (tonerAmount <= 0 || tonerAmount > 100){
+            return -1;
+        } else if (tonerLevel + tonerAmount > 100) {
+            return -1;
+        } else return (tonerLevel + tonerAmount);
+    }
+
+    public int printPages (int pages){
+        int pagesToPrint = (duplex) ? (pages / 2) + (pages % 2) : pages;
+        pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
