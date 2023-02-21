74da0493bd4f54875b7d10bc330c9177fbe6dfe9
Eric Jung <118792303+e11b@users.noreply.github.com>
2023-02-20 18:11:35 -0600

    L101 Comp part 1, deleted lesson for redo

--- Section8_OOP2_Composition/src/L101_Composition/PersonalComputer.java
+++ Section8_OOP2_Composition/src/L101_Composition/PersonalComputer.java
@@ -2,36 +2,27 @@ package L101_Composition;
 
 public class PersonalComputer extends Product{
 
+    private Chassis computerCase;
     private Monitor monitor;
-    private Chassis chassis;
     private Mobo mobo;
 
-    public PersonalComputer(String model, String manufacturer, Monitor monitor, Chassis chassis, Mobo mobo) {
+    public PersonalComputer(String model, String manufacturer,
+                            Chassis computerCase, Monitor monitor, Mobo mobo) {
         super(model, manufacturer);
+        this.computerCase = computerCase;
         this.monitor = monitor;
-        this.chassis = chassis;
         this.mobo = mobo;
     }
 
-    private void drawLogo(){
-        monitor.drawPixelAt(1200, 50, "yellow");
+    public Chassis getComputerCase() {
+        return computerCase;
     }
 
-    public void powerUp(){
-        chassis.pressPowerButton();
-        drawLogo();
+    public Monitor getMonitor() {
+        return monitor;
     }
 
-//    public Monitor getMonitor() {
-//        return monitor;
-//    }
-//
-//    public Chassis getChassis() {
-//        return chassis;
-//    }
-//
-//    public Mobo getMobo() {
-//        return mobo;
-//    }
-
+    public Mobo getMobo() {
+        return mobo;
+    }
 }
--- Section8_OOP2_Composition/src/L101_Composition/Product.java
+++ Section8_OOP2_Composition/src/L101_Composition/Product.java
@@ -1,6 +1,7 @@
 package L101_Composition;
 
 public class Product {
+
     private String model;
     private String manufacturer;
     private int width;
@@ -13,36 +14,36 @@ public class Product {
     }
 }
 
-class Monitor extends Product {
-
+class Monitor extends Product{
     private int size;
     private String resolution;
+
     public Monitor(String model, String manufacturer) {
         super(model, manufacturer);
     }
-
     public Monitor(String model, String manufacturer, int size, String resolution) {
         super(model, manufacturer);
         this.size = size;
         this.resolution = resolution;
     }
-
     public void drawPixelAt(int x, int y, String color){
-        System.out.println(String.format("Drawing pixel at %d, %d in color as %s ", x, y, color));
+        System.out.println(String.format(
+                "Drawing pixel at %d,%d in color %s ", x, y, color));
     }
-
 }
 
-class Mobo extends Product {
-
+class Mobo extends Product{
     private int ramSlots;
     private int cardSlots;
     private String bios;
+
+
     public Mobo(String model, String manufacturer) {
         super(model, manufacturer);
     }
 
-    public Mobo(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+    public Mobo(String model, String manufacturer, int ramSlots,
+                int cardSlots, String bios) {
         super(model, manufacturer);
         this.ramSlots = ramSlots;
         this.cardSlots = cardSlots;
@@ -52,12 +53,11 @@ class Mobo extends Product {
     public void loadProgram(String programName){
         System.out.println("Program " + programName + " is now loading...");
     }
-
 }
 
-class Chassis extends Product {
-
+class Chassis extends Product{
     private String powerSupply;
+
     public Chassis(String model, String manufacturer) {
         super(model, manufacturer);
     }
@@ -66,10 +66,9 @@ class Chassis extends Product {
         super(model, manufacturer);
         this.powerSupply = powerSupply;
     }
-
     public void pressPowerButton(){
         System.out.println("Power button pressed");
     }
-
 }
+
 
